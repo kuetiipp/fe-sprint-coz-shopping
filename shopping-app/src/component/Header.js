d72e7ad55fdc02233bd81f68015b35a1abedb3ac
kuetiipp <tjstjsgoqgoq@naver.com>
2023-07-13 02:12:01 +0900

    header clear

--- shopping-app/src/component/Header.js
+++ shopping-app/src/component/Header.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Main from "../pages/Main";
-import './header.css'
+import "./header.css";
 import { useState } from "react";
 import Dropdown from "./Dropdown";
 
 const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const showDropdown = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    const showDropdown = () => {
-        setIsMenuOpen(!isMenuOpen);
-    }
+  const logoUrl = "/logo.png";
+  const menuLogoUrl = "/dropdown.png";
+  const xUrl = "/x.png";
 
-    const logoUrl = "/logo.png";
-    const menuLogoUrl = "/dropdown.png";
-    const xUrl = "/x.png"
-
-    return (
-        <div className="header">
-            <Link to={Main}><img className="header-logo" src={logoUrl} alt="logo"></img></Link>
-            <div className="header-title">COZ Shopping</div>
-            <button className="header-menu-icon" onClick={showDropdown}>
-            {isMenuOpen ? 
-                <img src={menuLogoUrl} alt="menu"></img> : <img src={xUrl} alt="menu"></img>
-             }
-            </button>
-        </div>
-    );
-}
+  return (
+    <div className="header">
+      <Link to="/">
+        <img className="header-logo" src={logoUrl} alt="logo"></img>
+      </Link>
+      <div className="header-title">COZ Shopping</div>
+      <button className="header-menu-icon" onClick={showDropdown}>
+        {isMenuOpen ? (
+          <img className="closebtn" src={xUrl} alt="page-menu-close-butn"></img>
+        ) : (
+          <img className="openbtn" src={menuLogoUrl} alt="page-menu-open-butn"></img>
+        )}
+      </button>
+      {isMenuOpen && <Dropdown />}
+    </div>
+  );
+};
 
 export default Header;
